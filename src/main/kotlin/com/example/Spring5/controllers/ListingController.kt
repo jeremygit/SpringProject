adea14781805075390369714ab105cb21aa54da8
jeremy-nc <jeremy@nurturecloud.com>
2022-01-22 16:14:54 +1100

    add come comments

--- src/main/kotlin/com/example/Spring5/controllers/ListingController.kt
+++ src/main/kotlin/com/example/Spring5/controllers/ListingController.kt
@@ -54,3 +54,78 @@ sealed class ListingsFiniteResponse {
 data class ListingPutBody(
     val name: String
 )
+
+//
+//
+//
+
+//package com.example.Spring5.controllers
+//
+//import com.example.Spring5.entitys.ListingEntity
+//import com.example.Spring5.services.ListingService
+//import com.google.common.annotations.VisibleForTesting
+//import org.springframework.web.bind.annotation.*
+//
+//@RestController
+//@RequestMapping("/listings")
+//class ListingController(
+//    val listingService: ListingService
+//) {
+//    @GetMapping
+//    fun get(): ListingsFiniteResponse {
+//        ListingsFiniteResponse.Error("Error!")
+//        return ListingsFiniteResponse.SuccessEntity(listingService.getListings())
+//    }
+//
+//    // Hierarchy doesnt matter i.e. /{id} vs /save
+//    @GetMapping("/{id}")
+//    fun getListing(
+//        @PathVariable id:String
+//        // Renaming of path vars
+//        // @PathVariable("agencyId") agencyId: UUID,
+//        // @PathVariable("vaultContactId") vaultContactId: Long,
+//        // @PathVariable("vaultNoteId") vaultNoteId: Long,
+//    ): String {
+//        val listing = listingService.getListing()
+//        return "$id $listing"
+//    }
+//
+//    @GetMapping("/save")
+//    fun putListing(
+//        @RequestParam name: String
+//        // @RequestBody params: ListingPutBody
+//    ): String {
+//        return listingService.putListing(name)
+//    }
+//
+//    // Interesting visibility modifier/annotation
+//    // @VisibleForTesting
+//
+//    // applicationEventPublisher
+//}
+//
+//data class ListingsResponse(
+//    val listings: List<String>
+//)
+//
+//sealed class ListingsFiniteResponse {
+//
+//    data class SuccessString(
+//        val listings: List<String>
+//    ): ListingsFiniteResponse()
+//
+//    data class SuccessEntity(
+//        val listings: List<ListingEntity>
+//    ): ListingsFiniteResponse()
+//
+//    data class Error(
+//        val reason: String
+//    ): ListingsFiniteResponse()
+//
+//    object NoResponse: ListingsFiniteResponse()
+//
+//}
+//
+//data class ListingPutBody(
+//    val name: String
+//)
